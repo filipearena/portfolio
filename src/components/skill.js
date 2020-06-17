a03c20fa1c5b478a713bebb459622a339a5a741e
filipearena <filipe.arena@gmail.com>
2020-06-17 12:36:22 +1000

    section 3 flexbox to arrange skill position

--- src/components/skill.js
+++ src/components/skill.js
@@ -4,13 +4,17 @@ import React from "react";
 export default function Skill({ skillName, percentage }) {
   return (
     <div className="inner-container" style={{ border: '1px solid #000' }}>
-      <span className="inner-box-skill" style={{ display: 'block', width: '15%' }}>{skillName}</span>
+      <span className="inner-box-skill">{skillName}</span>
       <div className="inner-box-progress-bar" >
         <div style={{
           display: 'block', height: '20px', width: `${percentage}%`, backgroundColor: 'red'
-        }}> </div>
+        }}>{percentage}%</div>
+        <div
+          style={{
+            display: 'block', height: '20px', width: `${100 - percentage}%`, backgroundColor: 'grey'
+          }}></div>
       </div>
-    </div>
+    </div >
   );
 }
 
