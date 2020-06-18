06300d3257a8d005f23c5a0232dea50db6e2e56a
filipearena <filipe.arena@gmail.com>
2020-06-18 19:34:27 +1000

    transition to section 3 recommendations

--- src/components/recommendation.js
+++ src/components/recommendation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Recommendations({ text, author, position, download }) {
   return (
-    <a href={download} target="_blank" className="recommendation">
+    <a href={download} target="_blank" className="recommendation show-on-scroll">
       <div>"{text}"</div>
       <div style={{ marginTop: '12px' }}>- <b><i>{author}</i></b></div>
       <div><u>{position}</u></div>
