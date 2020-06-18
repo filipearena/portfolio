8728acc228650d379bdf79d2e27044ae048dfa8e
filipearena <filipe.arena@gmail.com>
2020-06-19 02:20:12 +1000

    changed presentation phrase

--- src/components/presentation.js
+++ src/components/presentation.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function Presentation() {
   return (
     <div className="presentation">
-      <h1 className="title show-on-scroll">
+      <h1 className="welcome show-on-scroll">
         Hello, I'm Filipe Arena
       </h1>
-      <h3 className="suggestion show-on-scroll">
-        <i>I'm a <u>Software Developer</u> with experience in React, VueJS, Angular and much more</i>
+      <h3 className="description show-on-scroll">
+        <i>an experienced <u>Software Developer</u> who has learned the value of hard work,
+        attention to detail and dedication along the years.</i>
       </h3>
-    </div>
+    </div >
   );
 }
 
